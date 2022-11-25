ed35b2127193d90c535557425378d5f64c86b703
isso0424 <isso04240129@gmail.com>
2022-11-25 16:10:06 +0900

    fix: lint error

--- src/response/repeat.go
+++ src/response/repeat.go
@@ -1,6 +1,9 @@
 package response
 
-import "nitic-pbl-2022-01/pbl-back/src/domain"
+import (
+	"nitic-pbl-2022-01/pbl-back/src/domain"
+	"strconv"
+)
 
 type Repeat struct {
   Unit string `json:"unit"`
@@ -13,7 +16,7 @@ func ConvertRepeat(repeat domain.Repeat) Repeat {
   return Repeat {
     Unit: repeat.Unit.Parse(),
     Value: repeat.Value,
-    Since: string(repeat.Since.Unix()),
-    Until: string(repeat.Until.Unix()),
+    Since: strconv.Itoa(int(repeat.Since.Unix())),
+    Until: strconv.Itoa((repeat.Until.Unix())),
   }
 }
