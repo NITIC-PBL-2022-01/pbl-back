cb1855049eae27788af98f77ee605bc7499c2918
isso0424 <isso04240129@gmail.com>
2022-11-25 15:34:27 +0900

    feat: define repeat converter

---
+++ src/response/repeat.go
@@ -0,0 +1,19 @@
+package response
+
+import "nitic-pbl-2022-01/pbl-back/src/domain"
+
+type Repeat struct {
+  Unit string `json:"unit"`
+  Value int `json:"value"`
+  Since string `json:"since"`
+  Until string `json:"until"`
+}
+
+func ConvertToRepeat(repeat domain.Repeat) Repeat {
+  return Repeat {
+    Unit: repeat.Unit.Parse(),
+    Value: repeat.Value,
+    Since: string(repeat.Since.Unix()),
+    Until: string(repeat.Until.Unix()),
+  }
+}
