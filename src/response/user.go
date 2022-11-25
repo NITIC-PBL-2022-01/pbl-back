0a7d7e84e7a4a4d7bca052b94d1aee8805119a9b
isso0424 <isso04240129@gmail.com>
2022-11-25 15:24:16 +0900

    feat: create user response converter

--- src/response/user.go
+++ src/response/user.go
@@ -1,16 +1,17 @@
 package response
 
-type user struct {
+import "nitic-pbl-2022-01/pbl-back/src/domain"
+
+type User struct {
   Email string `json:"email"`
   Name string `json:"name"`
   IsStudent bool `json:"is_student"`
 }
 
-// TODO: receive domain object for User
-func ParseToUser() user {
-  return user {
-    Email: "",
-    Name: "",
-    IsStudent: false,
+func ConvertToResponse(user domain.User) User {
+  return User {
+    Email: user.Email,
+    Name: user.Name,
+    IsStudent: user.IsStudent,
   }
 }
